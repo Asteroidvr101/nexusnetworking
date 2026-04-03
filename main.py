db5f27c28af05cfd9cc780f47809a4e8086a1b9e
Asteroidvr101 <projectinfectedoffical@gmail.com>
2026-04-03 10:47:05 -0500

    Update main.py

--- main.py
+++ main.py
@@ -13,9 +13,9 @@ async def send_voice(room_id: str, request: Request):
 
 @app.get("/get-voice/{room_id}")
 async def get_voice(room_id: str):
-    if room_id in voice_mailbox:
-        return Response(content=voice_mailbox[room_id], media_type="application/octet-stream")
-    return Response(status_code=404)
+    if room_id in voice_mailbox: # It only returns a 200 if this is TRUE
+        return Response(content=voice_mailbox[room_id], ...)
+    return Response(status_code=404) # Otherwise, it sends a 404
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=10000)
